c714aa2a34f300f5d79560059aa3e07a2d796eae
kiianskii <ankiyanskiy@gmail.com>
2024-02-23 15:26:50 +0200

    fix

--- js/task-1.js
+++ js/task-1.js
@@ -4,17 +4,11 @@
 // Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
 
 
-// На що буде звертати увагу ментор при перевірці:
-
-// Кількість категорій, їх назва та кількість елементів отримані за допомогою властивостей і методів DOM-елементів
-// Дані за кожною категорією були отримані й виведені в консоль у тілі циклу або методу forEach()
-// У консолі має бути виведено наступне повідомлення:
-
 const listEl = document.querySelectorAll(".js-item")
 console.log(`Number of categories: ${listEl.length}`)
 
 listEl.forEach((el) => {
-    console.log(`Category: ${el.firstElementChild.textContent}\n Elements: ${el.la.children.length}`)
+    console.log(`Category: ${el.firstElementChild.textContent}\n Elements: ${el.lastElementChild.children.length}`)
 })
 
 
--- js/task-3.js
+++ js/task-3.js
@@ -2,15 +2,6 @@
 
 
 
-// <input type="text" id="name-input" placeholder="Please enter your name" />
-// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
-
-// На що буде звертати увагу ментор при перевірці:
-
-// На елементі input#name-input прослуховується подія input
-// Під час набору тексту в інпуті його поточне значення підставляється в span#name-output як ім’я для привітання
-// Значення в інпуті очищене від пробілів по краях
-// Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous"
 
 const userNameElem = document.querySelector('#name-input');
 const titleElem = document.querySelector('#name-output');
