b663a610335ee55a8b94c1cf6afe0849f6ade705
kiianskii <ankiyanskiy@gmail.com>
2024-02-23 16:59:31 +0200

    upd

--- js/task-6.js
+++ js/task-6.js
@@ -80,37 +80,3 @@ function getRandomHexColor() {
 
 
 
-
-// destroyBtn.addEventListener("click", destroyBoxesClick);
-// createBtn.addEventListener("click", createBoxesClick);
-
-// function createBoxesClick() {
-//   let inputVal = inputNum.value;
-//   if (inputVal < 1 || inputVal > 100) {
-//     return alert("Number should be in range from 1 to 100");
-//   }
-//   destroyBoxesClick();
-
-//   let divArr = createBoxes(+inputVal);
-
-//   outputBoxes.prepend(...divArr);
-// }
-
-// function destroyBoxesClick() {
-//   outputBoxes.innerHTML = "";
-//   inputNum.value = "";
-// }
-
-// function createBoxes(amount) {
-//   const divArr = [];
-//   let size = 20;
-//   for (let i = 1; i <= amount; i++) {
-//     size += 10;
-//     const divBox = document.createElement("div");
-//     divBox.style.backgroundColor = getRandomHexColor();
-//     divBox.style.width = `${size}px`;
-//     divBox.style.height = `${size}px`;
-//     divArr.push(divBox);
-//   }
-//   return divArr;
-// }
